4b86ce8e9fff67689ce8fee036bced329c612fa3
michal <michal.czuba.1995@gmail.com>
2019-06-22 00:27:08 +0200

    added getByStudTeach?rspo=2889&dist=100 -> returns school with given coeff students to teachers, 2 schools with highest coess and two schools wiht lowest within a given area also getFullInfo?rspo=2889 -> returns full info of school with given rspo no

--- database_api/code/db_app.py
+++ database_api/code/db_app.py
@@ -4,14 +4,14 @@ from db_queries import DataHandler
 
 app = Flask(__name__)
 
-
+'''
 @app.route('/database_api/<place>')
 def get_place(place):
     dh = DataHandler()
     return dh.select_schools_by_place(place)
+'''
 
-
-@app.route('/database_api/get_by_distance')
+@app.route('/database_api/getByDistance')
 def get_by_distance():
     distance = request.args.get("dist")
     latitudeN = request.args.get("latN")
@@ -20,12 +20,51 @@ def get_by_distance():
     return dh.select_schools_by_distance(float(latitudeN), float(longitudeE), float(distance))
 
 
+@app.route('/database_api/getSchools')
+def get_schools():
+    sorted = request.args.get("sorted")
+    sort_by = request.args.get("sortby")
+    city = request.args.get("city")
+
+    where_cause = ''
+    sort_cause = ''
+
+    if sorted is 1:
+        sort_cause = 'ORDER BY '
+
+
+    return where_cause + sort_cause + "DUPA" + sorted
+
+
+
+@app.route('/database_api/getSimilarSchools')
+def get_similar_schools():
+    city = request.args.get("city")
+    id = 0
+    id = request.args.get("id")
+
+    return dh.get_similar_schools(str(city), int(id))
+
+
+@app.route('/database_api/getByStudTeach')
+def get_by_stud_teach():
+    rspo = request.args.get("rspo")
+    distance = request.args.get("dist")
+    dh = DataHandler()
+    return dh.select_school_by_stud_teach_coeff(rspo, float(distance))
+
+@app.route('/database_api/getFullInfo')
+def select_school_by_rspo():
+    rspo = request.args.get("rspo")
+    dh = DataHandler()
+    return dh.select_school_by_rspo(rspo)
+
 if __name__ == '__main__':
     app.run("0.0.0.0")
 
 
 """
 EXAMPLE OF USE
-http://127.0.0.1:5000/get_by_distance?dist=5&latN=51.504455&lonE=18.125713
-http://127.0.0.1:5000/Kalisz
+http://127.0.0.1:5000/database_api/getByDistance?dist=5&latN=51.504455&lonE=18.125713
+http://127.0.0.1:5000/database_api/Kalisz
 """
--- database_api/code/db_queries.py
+++ database_api/code/db_queries.py
@@ -5,7 +5,7 @@ from math import sin, cos, sqrt, atan2, radians
 
 class DataHandler:
 
-    def __init__(self, server='database:1433', user='sa', password='P@ssw0rd', database='BetterEducation'):
+    def __init__(self, server='database:1433', user='sa', password='P@ssw0rd', database='BetterEducation'): # zmienić na localhost
         self._conn = pymssql.connect(server=server, user=user, password=password, database=database)
 
     def _query_db(self, sql_query):
@@ -43,7 +43,7 @@ class DataHandler:
 
         result = list()
         for record in outer_places:
-            record_distance = self._get_distance(float(record['LatitudeN']), float(record['LongitudeS']),
+            record_distance = self._get_distance(float(record['LatitudeN']), float(record['LongitudeE']),
                                                  float(latidudeN), float(longitudeE))
             # print('{:7} {:7} {:7}\n'.format(record['RSPO'], center_place[0]['RSPO'], record_distance))
             if record_distance < distance:
@@ -51,10 +51,49 @@ class DataHandler:
 
         return json.dumps(result)
 
+    def select_school_by_stud_teach_coeff(self, rspo, distance):
+        query_skeleton = 'SELECT szkolyPlacowki.RSPO, CASE WHEN LEtatatowNauczycieli = 0 THEN 0 ELSE LUczniow /' \
+                         ' LEtatatowNauczycieli END \'StosunekUczNaucz\', LongitudeE, LatitudeN FROM szkolyPlacowki' \
+                         ' INNER JOIN szkolyAdresy sA on szkolyPlacowki.RSPO = sA.RSPO WHERE szkolyPlacowki.RSPO '
+        other_schools = self._query_db(query_skeleton + ' <> ' + rspo)
+        my_school = self._query_db(query_skeleton + ' = ' + rspo)
+
+        better_1 = my_school[0]
+        better_2 = my_school[0]
+
+        worse_1 = my_school[0]
+        worse_2 = my_school[0]
+
+        for record in other_schools:
+            record_distance = self._get_distance(float(record['LatitudeN']), float(record['LongitudeE']),
+                                                 float(my_school[0]['LatitudeN']), float(my_school[0]['LongitudeE']))
+            if record_distance <= distance:
+                if float(record['StosunekUczNaucz']) > float(my_school[0]['StosunekUczNaucz']):
+                    if float(record['StosunekUczNaucz']) > float(better_2['StosunekUczNaucz']):
+                        if record['StosunekUczNaucz'] > float(better_1['StosunekUczNaucz']):
+                            better_2 = better_1
+                            better_1 = record
+                        else:
+                            better_2 = record
+                else:
+                    if float(record['StosunekUczNaucz']) < float(worse_2['StosunekUczNaucz']):
+                        if float(record['StosunekUczNaucz']) < float(worse_1['StosunekUczNaucz']):
+                            worse_2 = worse_1
+                            worse_1 = record
+                        else:
+                            worse_2 = record
+
+        return json.dumps([worse_1, worse_2, my_school, better_2, better_1])
+
+    def select_school_by_rspo(self, rspo):
+        query_skeleton = 'SELECT * FROM szkolyAdresy INNER JOIN szkolyPlacowki sP on szkolyAdresy.RSPO = sP.RSPO WHERE' \
+                         ' szkolyAdresy.RSPO = '
+        my_school = self._query_db(query_skeleton + ' ' + rspo)
+        return json.dumps(my_school)
 
 
 
-"""
+'''
 # EXAMPLE OF USAGE
 dh = DataHandler()
 a = dh.select_schools_by_place('Hrubieszów')
@@ -62,4 +101,4 @@ b = dh.select_schools_by_distance(51.504455, 18.125713, 5) # wsp. Grabów n Pros
 
 print(a)
 print(b)
-"""
+'''
