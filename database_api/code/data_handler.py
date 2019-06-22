7ed4abf85f11f97b9ed6ec85d5d222f737c55c9f
damianbrzoza <damianbrzoza>
2019-06-22 10:47:11 +0200

    get_schools

--- database_api/code/data_handler.py
+++ database_api/code/data_handler.py
@@ -82,16 +82,22 @@ class DataHandler:
                 list_of_rspos.append(res['RSPO'])
 
             query_skeleton = 'SELECT RSPO FROM szkolyPlacowki WHERE RSPO IN '+ str(tuple(list_of_rspos)) + \
-                             'AND Typ = ' + str(' \''+ type + '\' ')+' '
+                             ' AND Typ = ' + str(' \''+ str(type) + '\' ' ) +' '
+
             result1 = self._query_db(query_skeleton)
+
             list_of_rspos = list()
             for res in result1:
                 list_of_rspos.append(res['RSPO'])
+            if len(list_of_rspos) != 0:
+                query_skeleton = 'SELECT RSPO, Miejscowosc, LatitudeN, LongitudeE, Ulica, NrDomu, KodPocztowy,' \
+                                 'Telefon, WWW, Email FROM szkolyAdresy WHERE RSPO IN ' + str(tuple(list_of_rspos)) +' '
 
-            query_skeleton = 'SELECT RSPO, Miejscowosc,LatitudeN, LongitudeE, Ulica, NrDomu, KodPocztowy,  ' \
-                             ' Telefon, WWW, Email FROM szkolyAdresy WHERE RSPO IN ' + str(tuple(list_of_rspos))
+                print(query_skeleton)
+                result = self._query_db(query_skeleton)
+            else:
+                result = []
 
-            result = self._query_db(query_skeleton)
         if sorted == 1:
             list_of_rspos = list()
             for res in result:
